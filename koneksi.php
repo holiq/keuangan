9a595f10d09979a95b86b5e6e55c7984b55af5de
holiq <holiq.ibrahim376@gmail.com>
2023-10-28 07:44:55 +0700

    feat: init laravel

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$koneksi = mysqli_connect('localhost', 'root', '', 'keuangan');
-
-if (mysqli_connect_errno()) {
-    echo 'koneksi gagal: '.mysqli_connect_error();
-} else {
-    echo 'koneksi sukses';
-}
